fdc9c44007e8229129feb120e734606a0622c557
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-10-23 11:52:48 -0700

    Fixed Bootstrap link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
+var modalEl = $("#exampleModal");
+var timeEl = $("span");
+
 setInterval(function () {
     var currentTime = moment().format("dddd, MMMM Do YYYY hh:mm:ss a");
-    var timeEl = $("span");
     timeEl.text(currentTime);
 }, 1000);
+
+$('#instructions-modal').on('click', function () {
+    console.log("hello")
+    modalEl.attr("aria-hidden", false);
+    console.log(modalEl)
+
+  })
